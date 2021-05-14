85801d9b60181688875842f0ef613bfa4da4396e
Thinh Nguyen <thinhntr@gmail.com>
2021-05-14 20:05:53 +0700

    Build the banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
+
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout 🛒</h1>
           </Route>
           <Route path="/login">
@@ -15,36 +18,7 @@ function App() {
           {/* This is the default route */}
           <Route path="/">
             <Header />
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
-            <h1>HOME PAGE!!!</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
