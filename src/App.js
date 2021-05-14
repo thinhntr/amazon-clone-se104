87dc68cd74f6f178348330e1d578a177e96f3ec4
Thinh Nguyen <thinhntr@gmail.com>
2021-05-14 11:21:07 +0700

    Frame of the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout 🛒</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page 🔑</h1>
+          </Route>
+          {/* This is the default route */}
+          <Route path="/">
+            <h1>HOME PAGE!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+{
+  /* Need react-router for switching between pages 👇 */
+}
+{
+  /* localhost.com */
+}
+{
+  /* localhost.com/checkout */
+}
+{
+  /* localhost.com/login */
 }
 
 export default App;
