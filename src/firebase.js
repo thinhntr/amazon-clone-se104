882cab431ecde3424229a57b828fd1f54cd04b5f
Thinh Nguyen <thinhntr@gmail.com>
2021-05-17 08:42:44 +0700

    Add config to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,14 @@
 import firebase from "firebase";
 
-const firebaseConfig = {}; // The config goes here
+const firebaseConfig = {
+  apiKey: "AIzaSyAajXdnzUiJoR0NVmQHd9_Mu64J-VqxryQ",
+  authDomain: "clone-se104.firebaseapp.com",
+  projectId: "clone-se104",
+  storageBucket: "clone-se104.appspot.com",
+  messagingSenderId: "615919209041",
+  appId: "1:615919209041:web:c06c8bf3b5aeb0f9290670",
+  measurementId: "G-YCB8LZP2TB",
+};
 
 firebase.initializeApp(firebaseConfig);
 
